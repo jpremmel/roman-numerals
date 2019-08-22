f5f6d28850bf4bd472f3ba85949669ffdb5c0d25
Jacqueline Remmel, Lauren Musante and sofia giordano <jacqueline@>
2019-08-22 15:05:09 -0700

    Delete placeArray and add loops to designate roman numerals for M, C, X, and I.

--- js/scripts.js
+++ js/scripts.js
@@ -7,35 +7,47 @@ $(document).ready(function() {
 });
 //BUSINESS LOGIC
 function convertToRoman(input) {
-  var output = "";
+  var m = "";
+  var d = "";
+  var c = "";
+  var l = "";
+  var x = "";
+  var v = "";
+  var i = "";
   var ones = "";
   var tens = "";
   var hundreds = "";
   var thousands = "";
+  var output = "";
+
   if (input > 3999 || input < 0) {
     alert("Please enter a number between 0 and 3,999");
   } else {
     var inputString = input.toString();
     var digitArray = inputString.split("");
-
-    if(digitArray[3]) {
-      digitArray[0] = digitArray[0] + "000";
-      digitArray[1] = digitArray[1] + "00";
-      digitArray[2] = digitArray[2] + "0";
-    } else if (digitArray[2]) {
-      digitArray[0] = digitArray[0] + "00";
-      digitArray[1] = digitArray[1] + "0";
-    } else if (digitArray[1]) {
-      digitArray[0] = digitArray[0] + "0"
-    }
     console.log(digitArray);
 
-
-    for(var i=0; i<input; i++){
-      ones += "I";
+    if (digitArray.length === 4) {
+      for (var n = 0; n < digitArray[0]; n++) {
+        m += "M";
+      }
+      console.log(m);
+    } else if (digitArray.length === 3) {
+      for (var n = 0; n < digitArray[0]; n++) {
+        c += "C";
+      }
+      console.log(c);
+    } else if (digitArray.length === 2) {
+      for (var n = 0; n < digitArray[0]; n++) {
+        x += "X";
+      }
+      console.log(x);
+    } else if (digitArray.length === 1) {
+      for (var n = 0; n < digitArray[0]; n++) {
+        i += "I";
+      }
+      console.log(i);
     }
-
-
   }
   output= thousands + hundreds + tens + ones;
   return output;
