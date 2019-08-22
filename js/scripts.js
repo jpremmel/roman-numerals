7767cbdacafcda908ca2864db63f287efc624593
Jacqueline Remmel, Lauren Musante and sofia giordano <sofia@>
2019-08-22 13:40:17 -0700

    Add javascript to split input into an array and add 0s to designate the numerical place.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,35 @@ $(document).ready(function() {
 //BUSINESS LOGIC
 function convertToRoman(input) {
   var output = "";
+  var ones = "";
+  var tens = "";
+  var hundreds = "";
+  var thousands = "";
   if (input > 3999 || input < 0) {
     alert("Please enter a number between 0 and 3,999");
   } else {
-    for(var i=0; i<input; i++){
-      output += "I";
+    var inputString = input.toString();
+    var digitArray = inputString.split("");
 
+    if(digitArray[3]) {
+      digitArray[0] = digitArray[0] + "000";
+      digitArray[1] = digitArray[1] + "00";
+      digitArray[2] = digitArray[2] + "0";
+    } else if (digitArray[2]) {
+      digitArray[0] = digitArray[0] + "00";
+      digitArray[1] = digitArray[1] + "0";
+    } else if (digitArray[1]) {
+      digitArray[0] = digitArray[0] + "0"
     }
+    console.log(digitArray);
+
+
+    for(var i=0; i<input; i++){
+      ones += "I";
+    }
+
+
   }
+  output= thousands + hundreds + tens + ones;
   return output;
 }
